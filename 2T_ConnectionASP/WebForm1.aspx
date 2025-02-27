e46662b2e93f98a161ba36574c7cbf2f0f5acd8b
Yashraj <upadhyayyashraj7@gmail.com>
2025-02-27 09:17:10 +0530

    chang

---
+++ 2T_ConnectionASP/WebForm1.aspx
@@ -0,0 +1,145 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_2T_ConnectionASP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+
+            <table border="3">
+                <tr>
+                    <td>Name:</td>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+
+                 </tr>
+
+                <tr>
+                    <td>Gender:</td>
+                    <td>
+                        <asp:RadioButtonList ID="rdbgd" runat="server" RepeatDirection="Horizontal">
+                            <asp:ListItem>Male</asp:ListItem>
+                            <asp:ListItem>Female</asp:ListItem>
+                        </asp:RadioButtonList></td>
+
+                 </tr>
+
+                <tr>
+                    <td>Hobbies:</td>
+                    <td>
+                        <asp:CheckBoxList ID="chkhb" runat="server" RepeatDirection="Horizontal">
+                            <asp:ListItem>Crickert</asp:ListItem>
+                            <asp:ListItem>Football</asp:ListItem>
+                            <asp:ListItem>Vallyball</asp:ListItem>
+                        </asp:CheckBoxList></td>
+
+                 </tr>
+                
+
+                <tr>
+                    <td>City:</td>
+                    <td>
+                        <asp:DropDownList ID="drpct" runat="server">
+                            <asp:ListItem>--Select City--</asp:ListItem>
+                            <asp:ListItem>Rajkot</asp:ListItem>
+                            <asp:ListItem>Gondal</asp:ListItem>
+                            <asp:ListItem>Dhoraji</asp:ListItem>
+                        </asp:DropDownList></td>
+
+                 </tr>
+
+                <tr>
+                    <td>Address:</td>
+                    <td>
+                        <asp:TextBox ID="txtadd" runat="server"></asp:TextBox></td>
+
+                 </tr>
+
+                <tr>
+                    <td>Image:</td>
+                    <td>
+                        <asp:FileUpload ID="fldimg" runat="server" /></td>
+
+                 </tr>
+
+                <tr>
+                    
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click1" /></td>
+
+                 </tr>
+            </table>
+                            </center>
+
+        </div>
+        <p style="margin-left: 760px">
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCommand="GridView1_RowCommand">
+                <Columns>
+                    <asp:TemplateField HeaderText="Id">
+                        <ItemTemplate>
+                            <asp:Label ID="Label1" runat="server" Text='<%# Eval("Id") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Name">
+                        <ItemTemplate>
+                            <asp:Label ID="Label2" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Gender">
+                        <ItemTemplate>
+                            <asp:Label ID="Label3" runat="server" Text='<%# Eval("Gender") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Hobby1">
+                        <ItemTemplate>
+                            <asp:Label ID="Label4" runat="server" Text='<%# Eval("Hobby1") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Hobby2">
+                        <ItemTemplate>
+                            <asp:Label ID="Label5" runat="server" Text='<%# Eval("Hobby2") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Hobby3">
+                        <ItemTemplate>
+                            <asp:Label ID="Label6" runat="server" Text='<%# Eval("Hobby3") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="City">
+                        <ItemTemplate>
+                            <asp:Label ID="Label7" runat="server" Text='<%# Eval("City") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Address">
+                        <ItemTemplate>
+                            <asp:Label ID="Label8" runat="server" Text='<%# Eval("Address") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Image">
+                        <ItemTemplate>
+                            <asp:Image ID="Image1" Height="50" Width="50" runat="server" ImageUrl='<%# Eval("Image") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Edit">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="LinkButton1" runat="server" CommandArgument='<%# Eval("Id") %>' CommandName="cmd_edt">Edit</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Remove">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="LinkButton2" runat="server" CommandArgument='<%# Eval("Id") %>' CommandName="cmd_dlt">Remove</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ 2T_ConnectionASP/WebForm1.aspx.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+
+namespace _2T_ConnectionASP
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        DataSet ds;
+        SqlDataAdapter da;
+        string s = ConfigurationManager.ConnectionStrings["dbconnect"].ToString();
+        string fnm,h1,h2,h3;
+        string[] hb = new string[3];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection();
+            fillg();
+
+        }
+        void fillg()
+        {
+            connection();
+            da = new SqlDataAdapter("select * from emp_tbl", con);
+            ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+        void connection()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            uploadimg();
+            connection();
+            hobbies();
+            if (Button1.Text=="Save")
+            {
+                
+
+                for(int i =0;i<1;i++)
+                {
+                    if(hb[i]=="Cricket")
+                    {
+                        h1 = "Cricket";
+                        i++;
+                    }
+                    else
+                    {
+                        h1 = "Null";
+                        i++;
+                    }
+                    if(hb[i]=="Football")
+                    {
+                        h2 = "Football";
+                        i++;
+
+                    }
+                    else
+                    {
+                        h2 = "Null";
+                        i++;
+                    }
+
+                    if(hb[i]== "Vallyball")
+                    {
+                        h3 = "Vallyball";
+                        i++;
+                    }
+                    else
+                    {
+                        h3 = "Null";
+                        
+                    }
+
+                }
+
+
+                cmd =new SqlCommand ("insert into emp_tbl(Name,Gender,Hobby1,Hobby2,Hobby3,City,Address,Image) " +
+                    "values ('"+txtname.Text+"','"+rdbgd.Text+"', '"+h1+"','"+h2+"','"+h3+"','"+drpct.SelectedValue+"','"+txtadd.Text+"','"+fnm+"')", con);
+                cmd.ExecuteNonQuery();
+                fillg();
+            }
+
+            else
+            {
+                connection();
+                hobbies();
+                for (int i = 0; i < 1; i++)
+                {
+                    if (hb[i] == "Cricket")
+                    {
+                        h1 = "Cricket";
+                        i++;
+                    }
+                    else
+                    {
+                        h1 = "Null";
+                        i++;
+                    }
+                    if (hb[i] == "Football")
+                    {
+                        h2 = "Football";
+                        i++;
+
+                    }
+                    else
+                    {
+                        h2 = "Null";
+                        i++;
+                    }
+
+                    if (hb[i] == "Vallyball")
+                    {
+                        h3 = "Vallyball";
+                        i++;
+                    }
+                    else
+                    {
+                        h3 = "Null";
+
+                    }
+
+                }
+
+                cmd = new SqlCommand("update emp_tbl set Name='"+txtname.Text+"',Gender='"+rdbgd.Text+"',Hobby1='"+h1+ "' ,Hobby2='" + h2 + "',Hobby3='" + h3 + "' ,City='"+drpct.SelectedValue+"',Address='"+txtadd.Text+"' where Id='"+ViewState["id"]+"'      ", con);
+                cmd.ExecuteNonQuery();
+                empty();
+                fillg();
+
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName=="cmd_edt")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                ViewState["id"] = id;
+                filltext();
+                Button1.Text = "Update";
+            }
+            else
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                ViewState["id"] = id;
+                cmd = new SqlCommand("delete from emp_tbl where Id='"+ViewState["id"]+"'", con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataSet select(int id)
+        {
+            connection();
+            da = new SqlDataAdapter("select * from emp_tbl where Id='"+id+"'", con);
+            ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        void filltext()
+        {
+            connection();
+            ds = new DataSet();
+            ds = select(Convert.ToInt32(ViewState["id"]));
+
+            txtadd.Text = ds.Tables[0].Rows[0]["Address"].ToString();
+            txtname.Text = ds.Tables[0].Rows[0]["Name"].ToString();
+            rdbgd.Text = ds.Tables[0].Rows[0]["Gender"].ToString();
+            drpct.SelectedValue = ds.Tables[0].Rows[0]["City"].ToString();
+
+            if(ds.Tables[0].Rows[0]["Hobby1"].ToString()=="Cricket")
+            {
+                chkhb.Items[0].Selected = true;
+            }
+            else
+            {
+                chkhb.Items[0].Selected = false;
+            }
+
+            if (ds.Tables[0].Rows[0]["Hobby2"].ToString() == "Football")
+            {
+                chkhb.Items[1].Selected = true;
+            }
+            else
+            {
+                chkhb.Items[1].Selected = false;
+            }
+
+            if (ds.Tables[0].Rows[0]["Hobby3"].ToString() == "Vallyball")
+            {
+                chkhb.Items[2].Selected = true;
+            }
+            else
+            {
+                chkhb.Items[2].Selected = false;
+            }
+        }
+
+        void empty()
+        {
+            txtname.Text = "";
+            txtadd.Text = "";
+            rdbgd.ClearSelection();
+            drpct.ClearSelection();
+            chkhb.ClearSelection();
+
+            Button1.Text = "Save";
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void uploadimg()
+        {
+            if(fldimg.HasFile)
+            {
+                fnm = "images/" + fldimg.FileName;
+                fldimg.SaveAs(Server.MapPath(fnm));
+            }
+        }
+
+        void hobbies()
+        {
+            for(int i = 0; i<hb.Length; i++)
+            {
+                if(chkhb.Items[i].Selected==true)
+                {
+                    hb[i] = chkhb.Items[i].Text;
+                }
+            }
+        }
+    }
+}
